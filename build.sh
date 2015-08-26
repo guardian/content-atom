446175643aa543b5e11c828f5da3491169030a53
Paul Roberts <paul.roberts@guardian.co.uk>
2015-08-26 12:53:20 +0100

    build.sh changes
    run through each file separately (as thrift doesn't seem to support
    multiple files at once?!)
    
    output send thrift output to a more easily ignoreable location

--- build.sh
+++ build.sh
@@ -2,27 +2,61 @@
 
 LANGUAGES=("js:node" "java")
 
-# if this is empty then thrift won't run in verbose mode
-VERBOSE="-v"
+THRIFT_VERBOSE="-v"
 
-THRIFT_DIR="thrift"
-: ${THRIFT_CMD:=$(which thrift)}
-
-shopt -s globstar
-THRIFT_FILES=($THRIFT_DIR/**/*.thrift)
+while getopts "q" OPT
+do
+  case $OPT in
+    "q") THRIFT_VERBOSE=""
+      ;;
+    "v") THRIFT_VERBOSE="-v"
+      ;;
+  esac
+done
 
 function fatal {
     echo >&2 $@
     exit 1
 }
 
-if [[ ! -x "${THRIFT_CMD}" ]]
-then
-    fatal "ERROR: Cannot find thrift. Either install it in the path or set variable" \
-          "THRIFT_CMD to its location. (See https://thrift.apache.org/download)"
-fi
+shopt -s globstar
+
+THRIFT_DIR="thrift"
+OUT_DIR="generated"
+JS_LIB_PATH="$OUT_DIR/gen-nodejs"
+
+function make_dir_or_fail () {
+    local DIR=$1
+    if [[ ! -d $DIR ]]
+    then
+        echo "Directory \"$OUT_DIR\" does not exist, creating"
+        if ! mkdir $DIR
+        then
+            fatal "Could not create output directory \"$DIR\""
+        fi
+    fi
+}
+
+function thrift {
+    make_dir_or_fail $OUT_DIR
+
+    : ${THRIFT_CMD:=$(which thrift)}
+
+    THRIFT_FILES=($THRIFT_DIR/**/*.thrift)
+
+    if [[ ! -x "${THRIFT_CMD}" ]]
+    then
+        fatal "ERROR: Cannot find thrift. Either install it in the path or set variable" \
+              "THRIFT_CMD to its location. (See https://thrift.apache.org/download)"
+    fi
 
 
-LANG_OPTS=${LANGUAGES[@]/#/--gen }
+    LANG_OPTS=${LANGUAGES[@]/#/--gen }
 
-$THRIFT_CMD $VERBOSE -o ${THRIFT_DIR} $LANG_OPTS ${THRIFT_FILES[@]}
+    for file in $THRIFT_FILES
+    do
+        $THRIFT_CMD $THRIFT_VERBOSE --recurse -o ${OUT_DIR} $LANG_OPTS "$file"
+    done
+}
+
+thrift
