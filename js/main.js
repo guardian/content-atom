649fdf291ed5d53aeae3d320e26f0709d187b4dc
Paul Roberts <paul.roberts@guardian.co.uk>
2015-08-26 13:37:33 +0100

    include the quiz/tenfour stuff in the main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
-exports.events = require('../thrift/gen-nodejs/events_types');
+exports.eventTypes = require('../generated/gen-nodejs/events_types');
+exports.tenfourTypes  = require('../generated/gen-nodejs/tenfour_types');
