2686b61ebce959ca6138f89e163a89a65cb1f2a4
Paul Roberts <paul.roberts@guardian.co.uk>
2015-08-28 10:05:48 +0100

    add types of content atom so they can fit into a union

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
+exports.contentAtomTypes = require('../generated/gen-nodejs/contentatom_types');
 exports.eventTypes = require('../generated/gen-nodejs/events_types');
 exports.tenfourTypes  = require('../generated/gen-nodejs/tenfour_types');
---
+++ thrift/contentatom.thrift
@@ -0,0 +1,5 @@
+include "atoms/tenfour.thrift"
+
+union ContentAtom {
+  1: tenfour.Content tenfour
+}
