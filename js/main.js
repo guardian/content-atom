8240d1942c6cf70cf9cff553ec1ca239667151f7
James Gorrie <james.gorrie@theguardian.com>
2016-06-21 10:52:38 +0100

    add media types to main

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 exports.contentAtomTypes = require('../generated/gen-nodejs/contentatom_types');
+exports.mediaTypes  = require('../generated/gen-nodejs/media_types');
 exports.quizTypes  = require('../generated/gen-nodejs/quiz_types');
 exports.shared = require('../generated/gen-nodejs/shared_types');
