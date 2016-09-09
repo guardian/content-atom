dfe07fcb292deb6929fec9ef49270931af8a6169
Paul Roberts <pmr@stelo.org.uk>
2016-09-09 10:46:49 +0100

    change name of cta atom to match 'convention' that macro code gen uses

--- thrift/src/main/thrift/atoms/cta.thrift
+++ thrift/src/main/thrift/atoms/cta.thrift
@@ -2,7 +2,7 @@ namespace * contentatom.cta
 namespace java com.gu.contentatom.thrift.atom.cta
 #@namespace scala com.gu.contentatom.thrift.atom.cta
 
-struct CTAAtom {
+struct CtaAtom {
     1: required string url
     2: optional string backgroundImage
     3: optional string btnText
