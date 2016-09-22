e0fd84aa70d5fda504f78113ac769028f98b89b7
Will Franklin <will.franklin@guardian.co.uk>
2016-09-22 17:30:43 +0100

    Add some field for now

--- thrift/src/main/thrift/atoms/interactive.thrift
+++ thrift/src/main/thrift/atoms/interactive.thrift
@@ -3,5 +3,6 @@ namespace java com.gu.contentatom.thrift.atom.interactive
 #@namespace scala com.gu.contentatom.thrift.atom.interactive
 
 struct InteractiveAtom {
-
+  /* the unique ID will be stored in the `atom` data*/
+  2: required string title
 }
