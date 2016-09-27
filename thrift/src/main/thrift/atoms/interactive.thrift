a0ed008fc6f333774493ad2c7e947f1c5435cbfc
Will Franklin <will.franklin@guardian.co.uk>
2016-09-27 14:23:23 +0100

    Proper atom definition

--- thrift/src/main/thrift/atoms/interactive.thrift
+++ thrift/src/main/thrift/atoms/interactive.thrift
@@ -4,5 +4,10 @@ namespace java com.gu.contentatom.thrift.atom.interactive
 
 struct InteractiveAtom {
   /* the unique ID will be stored in the `atom` data*/
-  2: required string title
+  2: required string type
+  3: required string css
+  4: required string html
+  5: optional string inlineJS
+  6: optional string mainJS
+  7: optional string docData
 }
