c68774733e15ff829ff815bdace0133a73f71247
Will Franklin <will.franklin@guardian.co.uk>
2016-10-04 15:22:14 +0100

    Remove unnecessary comma

--- thrift/src/main/thrift/atoms/interactive.thrift
+++ thrift/src/main/thrift/atoms/interactive.thrift
@@ -5,7 +5,7 @@ namespace java com.gu.contentatom.thrift.atom.interactive
 struct InteractiveAtom {
   /* the unique ID will be stored in the `atom` data*/
   2: required string type
-  3: required string title,
+  3: required string title
   4: required string css
   5: required string html
   6: optional string mainJS
