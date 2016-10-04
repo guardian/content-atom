eae524b587b54c413b40a3fa98c1eeea6c2773bd
Will Franklin <will.franklin@guardian.co.uk>
2016-10-04 16:06:34 +0100

    Fix numbering

--- thrift/src/main/thrift/atoms/interactive.thrift
+++ thrift/src/main/thrift/atoms/interactive.thrift
@@ -4,10 +4,10 @@ namespace java com.gu.contentatom.thrift.atom.interactive
 
 struct InteractiveAtom {
   /* the unique ID will be stored in the `atom` data*/
-  2: required string type
-  3: required string title
-  4: required string css
-  5: required string html
-  6: optional string mainJS
-  7: optional string docData
+  1: required string type
+  2: required string title
+  3: required string css
+  4: required string html
+  5: optional string mainJS
+  6: optional string docData
 }
