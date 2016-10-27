0df0380b6468001302864122acbf1a8f2e4dfe8c
Luke Taylor <luke.taylor@guardian.co.uk>
2016-10-27 10:11:02 +0100

    Content Atom - ensure js/main.js is up to date.
    The js/main.js file is missing quite a lot of the content atoms that should be there while the js library is languishing quite far behind at 2.1.1 to the 2.4.7 we're currently on. I've updated the version to 2.4.7 to bring in line rather than bumping from what it was previously on. Please shout if you'd prefer I did not do this.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 exports.contentAtomTypes = require('../generated/gen-nodejs/contentatom_types');
+exports.ctaTypes  = require('../generated/gen-nodejs/cta_types');
+exports.explainerTypes  = require('../generated/gen-nodejs/explainer_types');
+exports.interactiveTypes  = require('../generated/gen-nodejs/interactive_types');
 exports.mediaTypes  = require('../generated/gen-nodejs/media_types');
 exports.quizTypes  = require('../generated/gen-nodejs/quiz_types');
+exports.reviewTypes  = require('../generated/gen-nodejs/review_types');
 exports.shared = require('../generated/gen-nodejs/shared_types');
