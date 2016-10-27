afb8fd607eb049a4f615cec1577202b033709011
Mariot Chauvin <mariot.chauvin@gmail.com>
2016-10-27 23:07:17 +0100

    fix ordering

--- thrift/src/main/thrift/atoms/recipe.thrift
+++ thrift/src/main/thrift/atoms/recipe.thrift
@@ -2,17 +2,6 @@ namespace * contentatom.recipe
 namespace java com.gu.contentatom.thrift.atom.recipe
 #@namespace scala com.gu.contentatom.thrift.atom.recipe
 
-struct RecipeAtom {
-  /* the unique ID will be stored in the `atom` data*/
-  1: required string title
-  2: required Tags tags 
-  3: required Time time
-  4: optional Serves serves
-  5: required list<IngredientsList> ingredientsLists
-  6: required list<string> steps
-  7: required list<string> credits
-}
-
 struct Tags {
  1: required list<string> cuisine
  2: required list<string> category
@@ -31,14 +20,25 @@ struct Serves {
   3: required i16 to
 }
 
-struct IngredientsList {
-    1: optional string title
-    2: required list<Ingredient> ingredients
-}
-
 struct Ingredient {
     1: required string item
     2: optional string comment
     3: required double quantity
     4: optional string unit
+}
+
+struct IngredientsList {
+    1: optional string title
+    2: required list<Ingredient> ingredients
+}
+
+struct RecipeAtom {
+  /* the unique ID will be stored in the `atom` data*/
+  1: required string title
+  2: required Tags tags 
+  3: required Time time
+  4: optional Serves serves
+  5: required list<IngredientsList> ingredientsLists
+  6: required list<string> steps
+  7: required list<string> credits
 }
