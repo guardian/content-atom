18729f70892d157c135d833af2e61353a197fff2
Luke Taylor <luke.taylor@guardian.co.uk>
2017-02-21 13:02:08 +0000

    Content Atom - Adding 'story questions' atom.

---
+++ thrift/src/main/thrift/atoms/storyquestions.thrift
@@ -0,0 +1,27 @@
+namespace * contentatom.storyquestions
+namespace java com.gu.contentatom.thrift.atom.storyquestions
+#@namespace scala com.gu.contentatom.thrift.atom.storyquestions
+
+enum RelatedStoryLinkType {
+  TAG = 0,
+  STORY = 1,
+}
+
+struct Question {
+  1: required string questionId
+  2: required string questionText
+}
+
+struct QuestionSet {
+  1: required string questionSetId
+  2: required string questionSetTitle
+  3: required list<Question> questions
+}
+
+struct StoryQuestionsAtom {
+  1: required string relatedStoryId
+  2: required RelatedStoryLinkType relatedStoryLinkType 
+  3: required string title
+  4: optional list<QuestionSet> editorialQuestions
+  5: optional list<QuestionSet> userQuestions
+}
