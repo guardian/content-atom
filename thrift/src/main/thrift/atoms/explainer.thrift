243c441abc3d2aff700720d7216935cef848751d
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-30 15:46:01 +0100

    Create a separate SideNote atom

--- thrift/src/main/thrift/atoms/explainer.thrift
+++ thrift/src/main/thrift/atoms/explainer.thrift
@@ -2,67 +2,16 @@ namespace * contentatom.explainer
 namespace java com.gu.contentatom.thrift.atom.explainer
 #@namespace scala com.gu.contentatom.thrift.atom.explainer
 
-include "entity.thrift"
-include "../shared.thrift"
-
 enum DisplayType {
   FLAT = 0,
   EXPANDABLE = 1,
   CAROUSEL = 2
 }
 
-enum ExplainerType {
-  QA = 0,
-  GUIDE = 1,
-  TIMELINE = 2,
-  PROFILE = 3
-}
-
 struct ExplainerAtom {
-  1: optional string label
   /* the unique ID will be stored in the `atom` data*/
   2: required string title
-  /* hopefully we can get rid of this one later: */
   3: required string body
   4: required DisplayType displayType
   5: optional list<string> tags
-
-  6: optional ExplainerType explainerType
-  7: optional QAndAExplainerData qa
-  8: optional GuideExplainerData guide
-  9: optional TimelineExplainerData timeline
-  10: optional ProfileExplainerData profile
-}
-
-struct QAndAExplainerData {
-  1: optional Image eventImage
-  2: required list<ExplainerContentItem> items
-  3: optional string sidenote
-}
-
-struct GuideExplainerData {
-  1: optional Image eventImage
-  2: required list<ExplainerContentItem> items
-}
-
-struct TimelineExplainerData {
-  1: required list<ExplainerTimelineItem> events
-  3: optional Entity entity
-}
-
-struct ProfileExplainerData {
-  1: optional Image headshot
-  2: required list<ExplainerContentItem> items
-  3: optional Entity entity
-}
-
-struct ExplainerContentItem {
-  1: optional string title
-  2: required string body
-}
-
-struct ExplainerTimelineItem {
-  1: required string title
-  2: required DateTime date
-  3: optional string body
 }
---
+++ thrift/src/main/thrift/atoms/sidenote.thrift
@@ -0,0 +1,58 @@
+namespace * contentatom.sidenote
+namespace java com.gu.contentatom.thrift.atom.sidenote
+#@namespace scala com.gu.contentatom.thrift.atom.sidenote
+
+include "entity.thrift"
+include "../shared.thrift"
+
+enum SideNoteType {
+  QA = 0,
+  GUIDE = 1,
+  TIMELINE = 2,
+  PROFILE = 3
+}
+
+struct SideNoteAtom {
+  1: optional string typeLabel
+  2: required string title
+  3: optional list<string> tags
+
+  4: optional SideNoteType SideNoteType
+  5: optional QAndA qa
+  6: optional Guide guide
+  7: optional Timeline timeline
+  8: optional Profile profile
+}
+
+struct QAndA {
+  1: optional Image eventImage
+  2: required list<SideNoteContentItem> items
+  3: optional string sidenote
+}
+
+struct Guide {
+  1: optional Image eventImage
+  2: required list<SideNoteContentItem> items
+}
+
+struct Timeline {
+  1: required list<TimelineContentItem> events
+  3: optional Entity entity
+}
+
+struct Profile {
+  1: optional Image headshot
+  2: required list<SideNoteContentItem> items
+  3: optional Entity entity
+}
+
+struct SideNoteContentItem {
+  1: optional string title
+  2: required string body
+}
+
+struct TimelineContentItem {
+  1: required string title
+  2: required DateTime date
+  3: optional string body
+}
