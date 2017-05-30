0e431fda9baa4bdff83675d9a24fc2ae47a15da8
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-05-30 12:17:26 +0100

    Add all four possible explainer data structs

--- thrift/src/main/thrift/atoms/explainer.thrift
+++ thrift/src/main/thrift/atoms/explainer.thrift
@@ -15,4 +15,28 @@ struct ExplainerAtom {
   3: required string body
   4: required DisplayType displayType
   5: optional list<string> tags
+  6: optional ExplainerData explainerBody
+}
+
+union ExplainerData {
+  1: QAExplainerData qa
+  2: GuideExplainerData guide
+  3: TimelineExplainerData timeline
+  4: ProfileExplainerData profile
+}
+
+struct QAExplainerData {
+
+}
+
+struct GuideExplainerData {
+
+}
+
+struct TimelineExplainerData {
+
+}
+
+struct ProfileExplainerData {
+
 }
