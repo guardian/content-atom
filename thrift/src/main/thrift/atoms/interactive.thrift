3f22665400225ee62466dfcaff9a7abe09fee6d5
fweddi <freddiepreece@outlook.com>
2024-12-16 14:21:37 +0000

    Define CustomField

--- thrift/src/main/thrift/atoms/interactive.thrift
+++ thrift/src/main/thrift/atoms/interactive.thrift
@@ -3,6 +3,25 @@ namespace java com.gu.contentatom.thrift.atom.interactive
 #@namespace scala com.gu.contentatom.thrift.atom.interactive
 #@namespace typescript _at_guardian.content_atom_model.interactive
 
+union AnyValue {
+    1: bool boolValue
+    2: byte byteValue
+    3: i16 i16Value
+    4: i32 i32Value
+    5: i64 i64Value
+    6: double doubleValue
+    7: string stringValue
+    8: list<AnyValue> listValue
+    9: set<AnyValue> setValue
+    10: map<string, AnyValue> mapValue
+}
+
+struct CustomField {
+  1: required string fieldName
+  2: required string fieldType
+  3: required AnyValue defaultValue
+}
+
 struct InteractiveAtom {
   /* the unique ID will be stored in the `atom` data*/
   1: required string type
@@ -18,4 +37,5 @@ struct InteractiveAtom {
   See here: https://amp.dev/documentation/components/amp-iframe/ for more information.
    */
   7: optional string placeholderUrl
+  8: optional list<CustomField> customFields = []
 }
